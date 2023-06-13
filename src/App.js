520f483aa2bc8ba9550817c228b1c72a45b08418
gonzalezbri <4717bg@gmail.com>
2023-06-12 17:54:27 -0700

    updated

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import {Navbar,Nav,Button} from 'react-bootstrap';
+
+
+function TopBar() {
+  return (
+    <div className="top">
+      <Navbar bg="light" expand="lg" >
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src="https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg" alt="Instacart home" class="css-95cl80"></img> 
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
+
+    </div>
+  );
+}
+
+export default TopBar;
